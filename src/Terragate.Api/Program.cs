4b94c785a840b7ef0b5566a5e8517357cd9cc032
Matt Janda <MJanda@consilio.com>
2023-01-22 22:06:50 +0000

    Add comments to program

--- src/Terragate.Api/Program.cs
+++ src/Terragate.Api/Program.cs
@@ -1,19 +1,23 @@
 using Serilog;
 using System.Reflection;
 
+// Create web application builder.
 var builder = WebApplication.CreateBuilder(args);
 
+// Create serilog logger.
 var logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .CreateLogger();
 
+// Get application version.
 var assembly = Assembly.GetExecutingAssembly();
 var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
 
+// Log application version.
 logger.ForContext<Program>()
     .Information($"Starting {assembly.GetName().Name} {{version}}", version);
 
-
+// Use serilog for webhosting.
 builder.Host.UseSerilog(logger);
 
 // Add services to the container.
