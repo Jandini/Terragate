ae004afd4dfb8ac43881e42472349e73b363abee
Matt Janda <MJanda@consilio.com>
2023-02-09 21:35:49 +0000

    Add docker volume mapping to local run script

--- .run.ps1
+++ .run.ps1
@@ -1,7 +1,6 @@
 & .init.ps1
 
 dotnet publish -nologo --configuration Release --runtime linux-x64 --no-self-contained -p:OutputType=Exe -p:DebugOutput=None src\Terragate.sln
-
 if ($LASTEXITCODE -ne 0) {
     return
 }
@@ -9,5 +8,5 @@ if ($LASTEXITCODE -ne 0) {
 Start-Process "http://localhost:8088/swagger/index.html" -ErrorAction SilentlyContinue
 
 docker build -t $global:currentTag .
-docker run -p 8088:80 -it -e ASPNETCORE_ENVIRONMENT=Development -e TF_VAR_VRA_USER=$env:TF_VAR_VRA_USER -e TF_VAR_VRA_PASS=$env:TF_VAR_VRA_PASS --rm $global:currentTag
+docker run -p 8088:80 -it -v ${PWD}\.vra:/app/.vra -e ASPNETCORE_ENVIRONMENT=Development -e TF_VAR_VRA_USER=$env:TF_VAR_VRA_USER -e TF_VAR_VRA_PASS=$env:TF_VAR_VRA_PASS --rm $global:currentTag
 
