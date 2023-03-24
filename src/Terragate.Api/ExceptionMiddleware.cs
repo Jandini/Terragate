9a1124ea2662554176c33c2f2f9fc6a2944c7d3f
Matt Janda <matt.janda.kingston@gmail.com>
2023-03-24 01:40:43 +0000

    Add unhandled exception middlewared handler

---
+++ src/Terragate.Api/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using System.Net;
+using System.Net.Mime;
+
+namespace Terragate.Api
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "{unhandledExceptionMethod} {unhandledExceptionUrl}", 
+                    context.Request.Method, 
+                    context.Request.GetDisplayUrl());
+
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+        
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            
+            await context.Response.WriteAsJsonAsync(new 
+            {
+                context.Response.StatusCode,
+                exception.Message
+            });
+        }
+    }
+}
