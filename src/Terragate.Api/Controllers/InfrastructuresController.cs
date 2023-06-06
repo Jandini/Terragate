2ca3ca31a60f64c59718f2cfbc7a87ccc9fbb155
Matt Janda <matt.janda.kingston@gmail.com>
2023-06-06 12:34:08 +0100

    Use push property instead app context for infrastrucutre id

--- src/Terragate.Api/Controllers/InfrastructuresController.cs
+++ src/Terragate.Api/Controllers/InfrastructuresController.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Serilog.Context;
 using Terragate.Api.Models;
 using Terragate.Api.Services;
 
@@ -49,42 +50,43 @@ namespace Terragate.Api.Controllers
                 _logger.LogInformation($"Creating infrastructure from {files.Select(a => a.FileName)}");
                 infra = await _repository.AddInfrastructure(files);
 
-                // set id as string for serilog dynamic enricher
-                AppContext.SetData("InfrastructureId", infra.Id.ToString());
+                using (LogContext.PushProperty("InfrastructureId", infra.Id.ToString()))
+                {
+                    // add variables to command line if provided
+                    var values = (variables?.Split(';').Where(a => a.Trim() != string.Empty))?.ToArray() ?? Array.Empty<string>();
+                    var vars = values.Length > 0 ? " -var " + string.Join(" -var ", values) : string.Empty;
 
-                // add variables to command line if provided
-                var values = (variables?.Split(';').Where(a => a.Trim() != string.Empty))?.ToArray() ?? Array.Empty<string>();
-                var vars = values.Length > 0 ? " -var " + string.Join(" -var ", values) : string.Empty;
+                    await _terraform.StartAsync("init -no-color -input=false" + vars, infra.WorkingDirectory);
+                    await _terraform.StartAsync("apply -no-color -auto-approve -input=false" + vars, infra.WorkingDirectory);
 
-                await _terraform.StartAsync("init -no-color -input=false" + vars, infra.WorkingDirectory);
-                await _terraform.StartAsync("apply -no-color -auto-approve -input=false" + vars, infra.WorkingDirectory);
-
-                infra = _repository.GetInfrastructure(infra.Id);
-
-                if (!infra.Resources.Any())
-                    throw new TerraformException("Terraform completed successfully but no resourcre waw created.");
-
-
-                // It is possible that VM is still 'TurningOn'.
-                // Make sure all the resources are in 'On' status before give away the infrastructure.
-                int retry = 0;
-                while (infra.Resources.Any(a => a.Instances.Where(i => i.Status != "On").Any())) {
-                    var resources = infra.Resources.SelectMany(a => a.Instances.Where(i => i.Status != "On"));
-
-                    foreach (var resource in resources)
-                    {
-                        _logger.LogDebug($"Waiting for {resource.HostName} with status {resource.Status}...");
-                        await Task.Delay(1000);
-                    }
-
-                    await _terraform.StartAsync("refresh -no-color -input=false", infra.WorkingDirectory);
                     infra = _repository.GetInfrastructure(infra.Id);
 
-                    if (++retry > 10)
-                        throw new TerraformException("Resources are not ready.");
-                }
+                    if (!infra.Resources.Any())
+                        throw new TerraformException("Terraform completed successfully but no resourcre waw created.");
 
-                return Ok(_mapper.Map<InfrastructureDto>(infra));
+
+                    // It is possible that VM is still 'TurningOn'.
+                    // Make sure all the resources are in 'On' status before give away the infrastructure.
+                    int retry = 0;
+                    while (infra.Resources.Any(a => a.Instances.Where(i => i.Status != "On").Any()))
+                    {
+                        var resources = infra.Resources.SelectMany(a => a.Instances.Where(i => i.Status != "On"));
+
+                        foreach (var resource in resources)
+                        {
+                            _logger.LogDebug($"Waiting for {resource.HostName} with status {resource.Status}...");
+                            await Task.Delay(1000);
+                        }
+
+                        await _terraform.StartAsync("refresh -no-color -input=false", infra.WorkingDirectory);
+                        infra = _repository.GetInfrastructure(infra.Id);
+
+                        if (++retry > 10)
+                            throw new TerraformException("Resources are not ready.");
+                    }
+
+                    return Ok(_mapper.Map<InfrastructureDto>(infra));
+                }
             }
             catch (Exception)
             {
