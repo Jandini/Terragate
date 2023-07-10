80d73214855c54bbfccb100ea3548ff1574e6202
Matt Janda <matt.janda.kingston@gmail.com>
2023-07-10 17:34:25 +0100

    Disable build args in build script

--- docker-build.ps1
+++ docker-build.ps1
@@ -1,7 +1,9 @@
 $version = "$(dotnet gitversion /showvariable SemVer)", "1.0.0" | Select-Object -First 1
 Write-Host "Building jandini/terragate:$version..."
 # Build docker image 
-wsl docker build --build-arg VRA_HOST=$env:VRA_HOST --build-arg VRA_TENANT=$env:VRA_TENANT --build-arg VRA_USER=$env:VRA_USER --build-arg VRA_PASS=$env:VRA_PASS -t jandini/terragate:${version} .
+# Disabled build-arg. The values can be passed as runtime environment variables 
+# wsl docker build --build-arg VRA_HOST=$env:VRA_HOST --build-arg VRA_TENANT=$env:VRA_TENANT --build-arg VRA_USER=$env:VRA_USER --build-arg VRA_PASS=$env:VRA_PASS -t jandini/terragate:${version} .
+wsl docker build -t jandini/terragate:${version} .
 # Inspect docker image 
 wsl docker image inspect jandini/terragate:${version}
 
